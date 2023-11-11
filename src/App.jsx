9b4da926bf527d151f21b21a78f642a60c46db6e
Ismail Kilic <kilic.2000@hotmail.com>
2023-11-11 19:44:01 +0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import * as math from 'mathjs'
+import Button from './components/Button'
+import Input from './components/Input'
+
+function App() {
+  const [text, setText] = useState('');
+  const [result, setResult] = useState('');
+
+  const addToText = (val) => {
+    setText((text) => [...text, val])
+  }
+
+  const resetInput = () => {
+    setText(' ');
+    setResult(' ');
+  }
+
+  const calculateResult = () => {
+    const input = text.join('')
+    setResult(math.evaluate(input))
+  }
+
+  return (
+    <div className='App'>
+      <div className="calc-wrapper">
+        <Input text={text} result={result} />
+        <div className="row">
+          <Button symbol='7' handleClick={addToText} />
+          <Button symbol='8' handleClick={addToText} />
+          <Button symbol='9' handleClick={addToText} />
+          <Button symbol='/' color='#00AD85' handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol='4' handleClick={addToText} />
+          <Button symbol='5' handleClick={addToText} />
+          <Button symbol='6' handleClick={addToText} />
+          <Button symbol='*' color='#00AD85' handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol='1' handleClick={addToText} />
+          <Button symbol='2' handleClick={addToText} />
+          <Button symbol='3' handleClick={addToText} />
+          <Button symbol='+' color='#00AD85' handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol='0' handleClick={addToText} />
+          <Button symbol='.' handleClick={addToText} />
+          <Button symbol='=' handleClick={calculateResult} />
+          <Button symbol='-' color='#00AD85' handleClick={addToText} />
+        </div>
+        <Button symbol='Clear' color='red' handleClick={resetInput} />
+      </div>
+    </div>
+  )
+}
+
+export default App
